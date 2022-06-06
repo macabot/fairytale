ca798c6f10c436155fdbfcdf914efed82304c5c3
macabot <1422579+macabot@users.noreply.github.com>
2022-06-06 23:43:20 +0200

    Find assets to serve

--- cmd/fairytale/cmd/serve.go
+++ cmd/fairytale/cmd/serve.go
@@ -1,35 +1,119 @@
 package cmd
 
 import (
+	"encoding/json"
+	"fmt"
 	"net/http"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"strings"
+	"time"
 
 	"github.com/spf13/cobra"
+	"golang.org/x/mod/modfile"
+	"golang.org/x/mod/module"
 )
+
+type moduleList struct {
+	Dir   string
+	GoMod string
+}
+
+const fairytaleModPath = "github.com/macabot/fairytale"
+
+func handle(mainWasmPath, wasmExecJsPath, assetsDir string) func(http.ResponseWriter, *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var servePath string
+		switch r.URL.Path {
+		case "/main.wasm":
+			servePath = mainWasmPath
+		case "/wasm_exec.js":
+			servePath = wasmExecJsPath
+		default:
+			servePath = filepath.Join(assetsDir, filepath.FromSlash(r.URL.Path))
+			if r.URL.Path == "/" {
+				servePath = filepath.Join(servePath, "index.html")
+			}
+		}
+		servePath = filepath.Clean(servePath)
+		http.ServeFile(w, r, servePath)
+		fmt.Printf("[%s] %s %s --> %s\n", time.Now().Format(time.RFC3339), r.Method, r.URL, servePath)
+	}
+}
+
+func findAssetsDir(cmd *cobra.Command) string {
+	out, err := exec.Command("go", "list", "-json", "-m").Output()
+	cobra.CheckErr(err)
+	var mod moduleList
+	cobra.CheckErr(json.Unmarshal(out, &mod))
+
+	goMod, err := os.ReadFile(mod.GoMod)
+	cobra.CheckErr(err)
+	f, err := modfile.Parse(mod.GoMod, goMod, nil)
+	cobra.CheckErr(err)
+
+	var modVer *module.Version
+	for _, require := range f.Require {
+		if require.Mod.Path == fairytaleModPath {
+			if require.Indirect {
+				cmd.PrintErrf("Warning: '%s' should not be an indirect requirement in your go.mod file.")
+			}
+			modVer = &require.Mod
+			break
+		}
+	}
+	for _, replace := range f.Replace {
+		if replace.Old.Path == fairytaleModPath {
+			modVer = &replace.New
+			break
+		}
+	}
+	if modVer == nil {
+		cobra.CheckErr("Your go.mod file does not require " + fairytaleModPath)
+	}
+
+	var fairytaleDir string
+	if modfile.IsDirectoryPath(modVer.Path) {
+		if filepath.IsAbs(modVer.Path) {
+			fairytaleDir = modVer.Path
+		} else {
+			fairytaleDir = filepath.Join(mod.Dir, modVer.Path)
+		}
+	} else {
+		modCacheBytes, err := exec.Command("go", "env", "GOMODCACHE").Output()
+		cobra.CheckErr(err)
+		modCache := strings.TrimSuffix(string(modCacheBytes), "\n")
+		path := append([]string{modCache}, strings.Split(modVer.String(), "/")...)
+		fmt.Println("modCache", path)
+		fairytaleDir = filepath.Join(path...)
+	}
+	return filepath.Join(fairytaleDir, "cmd", "fairytale", "cmd", "assets")
+}
+
+func findWasmExecJsPath() string {
+	out, err := exec.Command("go", "env", "GOROOT").Output()
+	cobra.CheckErr(err)
+	goRoot := strings.TrimSuffix(string(out), "\n")
+	return filepath.Join(goRoot, "misc", "wasm", "wasm_exec.js")
+}
 
 // serveCmd represents the serve command
 var serveCmd = &cobra.Command{
-	Use:   "serve [address] [directory]",
-	Short: "Serve the fairytale application",
-	Long: `Serve the fairytale application on the given address. It will use the current directory if no directory is provided.
-
-Examples:
-  # Serve the current directory on :8080
-  $ fairytale serve :8080
-
-  # Serve the directory named my-fairytale-app on :8080
-  $ fairytale serve :8080 my-fairytale-app`,
-	Args: cobra.MatchAll(
-		cobra.MinimumNArgs(1),
-		cobra.MaximumNArgs(2),
-	),
-	RunE: func(cmd *cobra.Command, args []string) error {
+	Use:     "serve [address] [main.wasm]",
+	Short:   "Serve the fairytale application",
+	Long:    `Serve the fairytale application during development of your applications. The fairytale app will be served on the given address.`,
+	Example: "fairytale serve :8080 path/to/main.wasm",
+	Args:    cobra.ExactArgs(2),
+	Run: func(cmd *cobra.Command, args []string) {
 		address := args[0]
-		dir := "."
-		if len(args) >= 2 {
-			dir = args[1]
-		}
-		http.Handle("/", http.FileServer(http.Dir(dir)))
-		return http.ListenAndServe(address, nil)
+		mainWasmPath := args[1]
+
+		assetsDir := findAssetsDir(cmd)
+		wasmExecJsPath := findWasmExecJsPath()
+
+		http.HandleFunc("/", handle(mainWasmPath, wasmExecJsPath, assetsDir))
+		cobra.CheckErr(http.ListenAndServe(address, nil))
 	},
 }
 
--- go.mod
+++ go.mod
@@ -5,11 +5,13 @@ go 1.18
 require (
 	github.com/macabot/hypp v0.0.0
 	github.com/spf13/cobra v1.4.0
+	golang.org/x/mod v0.5.1
 )
 
 require (
 	github.com/inconshreveable/mousetrap v1.0.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
+	golang.org/x/xerrors v0.0.0-20191011141410-1b5146add898 // indirect
 )
 
 replace github.com/macabot/hypp => ../hypp
